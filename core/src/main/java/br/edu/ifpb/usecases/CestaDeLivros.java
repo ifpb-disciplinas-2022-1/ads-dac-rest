64128ce70d02ba989f7434d4f6bcab28bba486ed
Ricardo Job <sousajob@gmail.com>
2022-05-17 10:46:10 -0300

    Adicionando o Stateful e a Seleção de livros

--- core/src/main/java/br/edu/ifpb/usecases/CestaDeLivros.java
+++ core/src/main/java/br/edu/ifpb/usecases/CestaDeLivros.java
@@ -1,9 +1,15 @@
 package br.edu.ifpb.usecases;
 
+import javax.ejb.Remove;
+import javax.ejb.Stateful;
+import javax.ejb.StatefulTimeout;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+@Stateful
+@StatefulTimeout(unit = TimeUnit.MINUTES, value = 30)
 public class CestaDeLivros implements Serializable {
     private List<String> livros = new ArrayList<>();
     public void adicionar(String livro){
@@ -12,7 +18,10 @@ public class CestaDeLivros implements Serializable {
     public List<String> livrosNaCesta(){
         return this.livros;
     }
+    @Remove // concluindo as interações com este objeto
     public void finalizar(){
+        System.out.println("----Lista de Livros-----");
+        livros.forEach(System.out::println);
         this.livros = new ArrayList<>();
     }
     public void remover(String livro){
--- core/src/main/java/br/edu/ifpb/web/jsf/ControladorDeReservas.java
+++ core/src/main/java/br/edu/ifpb/web/jsf/ControladorDeReservas.java
@@ -5,36 +5,49 @@ import br.edu.ifpb.usecases.CestaDeLivros;
 
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.spi.CDI;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Named
 @SessionScoped
 public class ControladorDeReservas implements Serializable {
     private String livro;
-//    private List<String> livros = new ArrayList<>();
-    private CestaDeLivros cesta = new CestaDeLivros();
+    @Inject //no intancição do objeto (ControladorDeReservas)
+    private CestaDeLivros cesta;// = new CestaDeLivros(); //session-key: 90ac0300181f-ffffffffd231423f-0
     public String adicionar(){
-//        livros.add(livro);
-        cesta.adicionar(livro);
+        cesta.adicionar(livro); //session-key: 90ac0300181f-ffffffffd231423f-0
         livro = "";
         return null;
+    }
+    public List<String> livrosDisponiveis(){
+        return Arrays.asList(
+            "livro 1",
+            "livro 2",
+            "livro 3",
+            "livro 4",
+            "livro 5"
+        );
     }
     public List<String> livrosNaCesta(){
         return this.cesta.livrosNaCesta();
     }
     public String finalizar(){
         this.cesta.finalizar();
-        this.cesta = new CestaDeLivros();
+//        this.cesta = new CestaDeLivros();
+        // recuperando uma nova instâcia do objeto
+        this.cesta = CDI.current()
+                .select(CestaDeLivros.class)
+                .get();
         return null;
     }
     public String remover(String livro){
-//        this.livros.remove(livro);
         this.cesta.remover(livro);
         return null;
-
     }
     public String getLivro() {
         return livro;
